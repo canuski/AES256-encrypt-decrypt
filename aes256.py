9de54611d1d95769abf5034c2c8a6c230a713b90
canuski <125011800+canuski@users.noreply.github.com>
2024-02-10 18:21:14 +0100

    Add comments for encryption

--- aes256.py
+++ aes256.py
@@ -1,27 +1,27 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes # Import klassen en modules die nodig zijn voor AES-256 encryptie en decryptie
+from cryptography.hazmat.backends import default_backend # Importeer de standaard backend van de cryptography module
+from cryptography.hazmat.primitives import padding # Importeer de padding module van de cryptography module
 import os
 
 
-def encrypt_aes_256(plain_text, key):
-    backend = default_backend()
-    iv = os.urandom(16)  # Generate a random initialization vector
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    encryptor = cipher.encryptor()
+def encrypt_aes_256(plain_text, key): 
+    backend = default_backend() # Environment set up waar de encryptie plaatsvindt
+    iv = os.urandom(16)  # Gen random 128-bit IV -> random waarde die ervoor zorgt dat er niet 2x dezelfde ciphertext wordt gegenereerd, nodig voor decryptie
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend) # Creeer een AES-256 cipher object met CBC mode
+    encryptor = cipher.encryptor() # Creeer een encryptor object
 
-    # Pad the data before encryption
+    # Pad de data, padding is nodig omdat block cipher input data moet zijn van een bepaalde grootte
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(plain_text.encode()) + padder.finalize()
 
-    # Encrypt the data
+    # Encrypt de data
     ciphertext = encryptor.update(padded_data) + encryptor.finalize()
 
     return iv + ciphertext
 
 
 def decrypt_aes_256(encrypted_data, key):
-    backend = default_backend()
+    backend = default_backend() # Environment set up waar de decryptie plaatsvindt
     iv = encrypted_data[:16]  # Extract the IV from the encrypted data
     ciphertext = encrypted_data[16:]  # Extract the ciphertext
 
